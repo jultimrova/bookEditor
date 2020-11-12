1c5a439e427401c9da53f86c614474d39620a5da
jultimrova@gmail.com <splntrs019>
2020-11-12 22:31:38 +0200

    add book

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const booksArray = JSON.parse(localStorage.getItem('books')) || []
+let booksArray = JSON.parse(localStorage.getItem('books') || [])
 
 const root = document.getElementById('root')
 root.className = 'root'
@@ -41,6 +41,69 @@ bookList.addEventListener('click', e => {
         }
     }
 })
+
+bookListContainer.addEventListener('click', e => {
+    if (e.target.classList.contains('btn-add')) {
+        addBookForm()
+
+        const addForm = document.getElementById('add-book-form')
+
+        addForm.onsubmit = e => {
+            e.preventDefault()
+
+            const titleValue = addForm.querySelector('#add-book-title').value
+            const authorValue = addForm.querySelector('#add-book-author').value
+            const imageValue = addForm.querySelector('#add-book-image').value
+            const plotValue = addForm.querySelector('#add-book-plot').value
+
+            let book = {
+                id: booksArray.length + 1,
+                title: titleValue,
+                author: authorValue,
+                imageLink: imageValue,
+                plot: plotValue
+            }
+
+            booksArray.push(book)
+            localStorage.setItem('books', JSON.stringify(booksArray))
+
+            const bookList = document.querySelector('.book-list')
+
+            bookList.insertAdjacentHTML('beforeend', `
+                <a href="#preview" id="${book.id}" class="book-list__title">${book.title}</a>
+                <a href="#edit" id="${book.id}" class="btn btn-edit">Edit</a>
+            `)
+
+            bookPreviewContainer.innerHTML = ''
+        }
+    }
+})
+
+
+function addBookForm() {
+    bookPreviewContainer.innerHTML = null
+
+    bookPreviewContainer.insertAdjacentHTML('beforeend', `
+        <div class="add-book">
+            <h3>Add book</h3>
+            <form id="add-book-form" class="add-book-form">
+                <label for="add-book-title">Book's title: </label>
+                <input type="text" id="add-book-title" name="title" class="input-field" value='' required>
+                <label for="add-book-author">Book's author: </label>
+                <input type="text" id="add-book-author" name="author" class="input-field" value='' required>
+                <label for="add-book-image">Book's image: </label>
+                <input type="url" id="add-book-image" name="image" class="input-field" value='' required>
+                <label for="add-book-plot">Book's description: </label>
+                <textarea id="add-book-plot" name="plot" class="input-field" rows="10" required></textarea>
+                <div class="btn-group">
+                    <button type="submit" class="btn btn-save">Save</button>
+                    <button type="button" id="add-book__cancel" class="btn btn-cancel">Cancel</button>
+                </div>
+            </form>
+        </div>
+    `)
+    history.pushState({}, 'add book', '/bookEditor/src/index.html#add')
+}
 
 function renderAllBooks() {
     for (let {id, title, author, imageLink, plot} of booksArray) {
