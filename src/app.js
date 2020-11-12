602d6e35ea92287d55762de3d2fa874c1b7feca4
jultimrova@gmail.com <splntrs019>
2020-11-12 16:29:48 +0200

    render all books in dynamic preview section

--- src/app.js
+++ src/app.js
@@ -17,12 +17,38 @@ const addButton = document.createElement('a')
 addButton.classList.add('btn', 'btn-add')
 addButton.innerHTML = 'Add'
 
+window.history.pushState('', 'main page', '/FL14_HW16/homework/index.html')
+
 root.append(bookListContainer)
 bookListContainer.append(listHeading)
 renderBookList(booksArray, bookListContainer)
 bookListContainer.appendChild(addButton)
 root.append(bookPreviewContainer)
+renderAllBooks()
 
+
+function renderAllBooks() {
+    for (let {id, title, author, imageLink, plot} of booksArray) {
+        if (booksArray.length > 0) {
+            renderBookPreview({id, title, author, imageLink, plot}, bookPreviewContainer)
+        }
+    }
+}
+
+function renderBookPreview({id, title, author, imageLink, plot}, node) {
+    const bookPrev = `
+        <div class="book-preview book" id="${id}">
+            <h3 class="book__heading">${title}</h3>
+            <img class="book__img" src="${imageLink}" alt="book image">
+            <div class="book__title">Title: ${title}</div>
+            <div class="book__author">Author: ${author}</div>
+            <div class="book-list__plot">Description: ${plot}</div>
+        </div>
+    `
+
+    node.insertAdjacentHTML('beforeend', bookPrev)
+    history.pushState({}, 'add book', `/FL14_HW16/homework/index.html#preview?id=${id}`)
+}
 
 function renderBookList(data, node) {
     let nav = `<nav class="book-list">`
